68e619a3b22de7fa0895d9ac2437226c5e1e239a
Nathan da Silva Barrozo <email.do.natann@gmail.com>
2023-09-15 22:17:57 -0300

    1

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,20 +1,47 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        analisarCandidato(2500.0);
-        analisarCandidato(2000.0);
-        analisarCandidato(1900.0);
+        selecaoCandidatos();
     }
 
-    static void analisarCandidato(double salarioPretendido) {
+    static void selecaoCandidatos() {
+        String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA",
+                "DANIELA", "JOSE" };
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
         double salarioBase = 2000.0;
-        if (salarioBase > salarioPretendido) {
-            System.out.println("LIGAR PARA CANDIDATO");
-        } else if (salarioBase == salarioPretendido) {
-            System.out.println("LIGAR PARA CANDIDATO COM CONTRA PROPOSTA");
-        } else {
-            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O cadidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+
+            candidatoAtual++;
         }
 
     }
 
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2500);
+    };
+
+    /*
+     * static void analisarCandidato(double salarioPretendido) {
+     * double salarioBase = 2000.0;
+     * if (salarioBase > salarioPretendido) {
+     * System.out.println("LIGAR PARA CANDIDATO");
+     * } else if (salarioBase == salarioPretendido) {
+     * System.out.println("LIGAR PARA CANDIDATO COM CONTRA PROPOSTA");
+     * } else {
+     * System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+     * }
+     * }
+     */
 }
